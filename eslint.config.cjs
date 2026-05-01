143c62c9af554275ebfffd44cce8ae9ed2e5cfcd
Red Elh <redaelhiri9@gmail.com>
2026-05-01 19:59:18 +0100

    fix: align eslint with CRA

--- eslint.config.cjs
+++
@@ -1,9 +0,0 @@
-const { FlatCompat } = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-module.exports = [
-  ...compat.extends("react-app", "react-app/jest"),
-];
