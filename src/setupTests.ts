785cee207fe55f0240f1402f9e73f3e9e2481cf2
Red Elh <redaelhiri9@gmail.com>
2026-07-21 18:11:35 +0100

    fix: resolve Jest test failure (TextEncoder + ESM module mocking)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,35 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import './i18n/config';
+import { TextEncoder, TextDecoder } from 'util';
+
+if (!global.TextEncoder) {
+  global.TextEncoder = TextEncoder;
+}
+if (!global.TextDecoder) {
+  global.TextDecoder = TextDecoder as any;
+}
+
+jest.mock('react-day-picker', () => ({
+  DayPicker: (props: any) => null,
+  DateRange: {},
+  DayPickerProvider: (props: any) => null,
+}));
+
+jest.mock('jspdf', () => ({
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => ({
+    text: jest.fn(),
+    save: jest.fn(),
+    output: jest.fn(),
+    internal: { pageSize: { getWidth: jest.fn(() => 210), getHeight: jest.fn(() => 297) } },
+  })),
+}));
+
+jest.mock('html2canvas', () => ({
+  __esModule: true,
+  default: jest.fn().mockResolvedValue({}),
+}));
 
 jest.mock('axios', () => ({
 	__esModule: true,
