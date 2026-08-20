00e28c3b46b19ac8aadbafd3f8fb08e8f832c059
Red Elh <redaelhiri9@gmail.com>
2026-08-20 20:04:20 +0100

    fix: remove bottom white blur gradient from owners hero section

--- src/pages/clients/Owners.tsx
+++ src/pages/clients/Owners.tsx
@@ -152,8 +152,6 @@ const Owners: React.FC = () => {
           </div>
         </div>
 
-        {/* Bottom Gradient */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white via-white/50 to-transparent"></div>
       </section>
 
       {/* Benefits Section */}
