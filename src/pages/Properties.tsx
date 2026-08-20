a7d6f9ea65dbbcd5918a062ac23fa61b0fa7b4ec
Red Elh <redaelhiri9@gmail.com>
2026-08-20 19:58:45 +0100

    fix: remove bottom white blur gradient from hero section

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -1149,8 +1149,6 @@ const Properties: React.FC = () => {
           </div>
         </div>
 
-        {/* Bottom Gradient */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white via-white/50 to-transparent"></div>
       </section>
 
 
